f4a517823fd0401acebcb4cda0dc619d0e11cd46
Nuttiwut Simsiri <nothsmac@Nuttiwuts-Mac-mini.local>
2023-08-25 23:28:06 +0700

    Fix error

--- libs/GGDFL.ts
+++ libs/GGDFL.ts
@@ -1,5 +1,32 @@
 'use server'
-import { drive } from "./googleDrive"
+import { google } from "googleapis"
+
+type GGResponeType = {
+    kind: string
+    mimeType: string
+    id: string
+    name: string
+
+}
+
+// authenticates the service account to be used in this context
+const auth = new google.auth.GoogleAuth({
+    // your credentials to authenticate
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY
+    },
+    // the actions you are permissed to perform using this API, in this case
+    // all CRUD operations are permissed, check out
+    // [ https://developers.google.com/drive/api/guides/api-specific-auth ]
+    // for more advice on scopes
+    scopes: ["https://www.googleapis.com/auth/drive"],
+})
+
+const drive = google.drive({
+    version: "v3",
+    auth: auth,
+})
 
 
 export const getFiles = async (folderId: string) => {
--- libs/googleDrive.ts
+++ libs/googleDrive.ts
@@ -1,5 +1,5 @@
 "use server"
-import { google } from "googleapis"
+
 
 type GGResponeType = {
     kind: string
@@ -9,24 +9,7 @@ type GGResponeType = {
 
 }
 
-// authenticates the service account to be used in this context
-const auth = new google.auth.GoogleAuth({
-    // your credentials to authenticate
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY
-    },
-    // the actions you are permissed to perform using this API, in this case
-    // all CRUD operations are permissed, check out
-    // [ https://developers.google.com/drive/api/guides/api-specific-auth ]
-    // for more advice on scopes
-    scopes: ["https://www.googleapis.com/auth/drive"],
-})
 
-const drive = google.drive({
-    version: "v3",
-    auth: auth,
-})
 
 export const getUrlsFrom = async (folderName: string) => {
     const ggUrl = `${process.env.DOMAIN_NAME}/api/google-drive`
