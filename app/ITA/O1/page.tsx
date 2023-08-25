d858546cd3273147f74683f5b301f4ef033559a3
Nuttiwut Simsiri <nothsmac@Nuttiwuts-Mac-mini.local>
2023-08-25 23:42:17 +0700

    debug ITA-O1 page

--- app/ITA/O1/page.tsx
+++ app/ITA/O1/page.tsx
@@ -1,3 +1,4 @@
+
 import GGDriveIframe from "@/components/ITA/GGDriveIframe"
 import ITABanner from "@/components/ITA/ITABanner"
 import { getUrlsFrom } from "@/libs/googleDrive"
@@ -9,6 +10,7 @@ export default async function O1Page() {
     ImageUrls
   } = await getUrlsFrom("O1 โครงสร้าง")
 
+  console.log("getUrlsFrom ::: ", PdfUrls, ImageUrls)
   return (
     <div className="flex flex-col items-center gap-4 justify-center w-full bg-gradient-to-b from-red-200 to-red-100">
       <ITABanner title="" customBanner={O1Banner} bgGradient="bg-gradient-to-r from-red-900 via-red-600 to-red-50"/>
