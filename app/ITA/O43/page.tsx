7dff15084ebfd297a5a39649335a2d700d405289
Nuttiwut Simsiri <nothsmac@Nuttiwuts-Mac-mini.local>
2023-08-28 12:20:27 +0700

    Add auto-gen ITA

--- app/ITA/O43/page.tsx
+++ app/ITA/O43/page.tsx
@@ -24,3 +24,4 @@ export default async function O43Page() {
   )
 }
 
+export const revalidate = 300
---
+++ app/ITA/[ITAID]/page.tsx
@@ -0,0 +1,28 @@
+import O1Banner from "@/public/ITABanner/B-O1.jpg"
+import GGDriveIframe from "@/components/ITA/GGDriveIframe"
+import ITABanner from "@/components/ITA/ITABanner"
+import { getUrlsFrom, GGResponeType } from "@/libs/googleDrive"
+
+
+export default async function AutoGenPage({ params }: { params: { ITAID: string } }) {
+  const {
+    PdfUrls,
+    ImageUrls
+  } = await getUrlsFrom(`${params.ITAID} `)
+
+  return (
+    <div className="flex flex-col items-center gap-4 justify-center w-full bg-gradient-to-b from-red-200 to-red-100">
+      <ITABanner title="" customBanner={O1Banner} bgGradient="bg-gradient-to-r from-red-900 via-red-600 to-red-50"/>
+
+      {
+        PdfUrls ? PdfUrls.map((pdfUrl : GGResponeType) => {
+          return <GGDriveIframe url={`https://drive.google.com/file/d/${pdfUrl.id}/preview`} name={pdfUrl.name} key={pdfUrl.id} />
+        })
+        : <></>
+      }
+
+    </div>
+  )
+}
+export const revalidate = 300
+
