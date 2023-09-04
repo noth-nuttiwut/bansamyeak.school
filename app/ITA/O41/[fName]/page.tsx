3407bc74c2f48430c576bb7acb8dd34957238b8d
Nuttiwut Simsiri <nothsmac@Nuttiwuts-Mac-mini.local>
2023-09-04 13:51:54 +0700

    change O41 Page Structure

---
+++ app/ITA/O41/[fName]/page.tsx
@@ -0,0 +1,27 @@
+import GGDriveIframe from "@/components/ITA/GGDriveIframe"
+import ITABanner from "@/components/ITA/ITABanner"
+import { getUrlsFrom } from "@/libs/googleDrive"
+import O25F1Banner from "@/public/ITABanner/B-O25.jpg"
+
+export default async function O25F1Page({ params }: { params: { fName: string } }) {
+  const {
+    PdfUrls,
+    ImageUrls
+  } = await getUrlsFrom(decodeURI(params.fName))
+
+  return (
+    <div className="flex flex-col items-center gap-4 justify-center w-full bg-gradient-to-b from-red-200 to-red-100">
+      <ITABanner title="" customBanner={O25F1Banner} bgGradient="bg-gradient-to-r from-red-900 via-red-600 to-red-50" customGoto="/ITA/O25"/>
+
+      {
+        PdfUrls ? PdfUrls.map((pdfUrl) => {
+          return <GGDriveIframe url={`https://drive.google.com/file/d/${pdfUrl.id}/preview`} name={pdfUrl.name} key={pdfUrl.id} />
+        })
+        : <></>
+      }
+
+    </div>
+  )
+}
+
+export const revalidate = 300
--- app/ITA/O41/page.tsx
+++ app/ITA/O41/page.tsx
@@ -1,31 +1,34 @@
-import ImageGalleryComp from "@/components/ImageGallery"
-import GGDriveIframe from "@/components/ITA/GGDriveIframe"
 import ITABanner from "@/components/ITA/ITABanner"
-import { getUrlsFrom } from "@/libs/googleDrive"
 import O1Banner from "@/public/ITABanner/B-O41.jpg"
+import { getFolderByName } from "@/libs/googleDrive"
+import Link from "next/link"
 
 export default async function O41Page() {
-  const {
-    PdfUrls,
-    ImageUrls
-  } = await getUrlsFrom("O41 ")
-
+  const allFolder = await getFolderByName("O41 ")
+  const newOrderImages = ["1", "2", "3", "4", "5"].map(id => allFolder?.find(el => el.name?.startsWith(id)))
   return (
-    <div className="flex flex-col items-center gap-4 justify-center w-full bg-gradient-to-b from-yellow-100 to-red-100">
-      <ITABanner title="" customBanner={O1Banner} bgGradient="bg-gradient-to-r from-amber-900 via-yellow-600 to-slate-50" />
-      {
-        ImageUrls.length > 1 ? <ImageGalleryComp images={ImageUrls}/>
-        : <></>
-      }
-      {
-        PdfUrls ? PdfUrls.map((pdfUrl) => {
-          return <GGDriveIframe url={`https://drive.google.com/file/d/${pdfUrl.id}/preview`} name={pdfUrl.name} key={pdfUrl.id} />
-        })
-          : <></>
-      }
+    <div className="flex flex-col items-center gap-4 justify-center w-full bg-gradient-to-b from-red-200 to-red-100">
+      <ITABanner title="" customBanner={O1Banner} bgGradient="bg-gradient-to-r from-red-900 via-red-600 to-red-50"/>
+
+      <div className="flex flex-wrap gap-6 p-8 sm:p-4 md:p-10 lg:p-20 xl:p-20 justify-center items-center">
+        {
+          newOrderImages.map((fd, i) => {
+            return (
+              <Link href={`/ITA/O41/${fd?.name}`} key={fd?.id}>
+                  <div className="flex bg-gradient-to-r text-slate-900 from-yellow-500  to-yellow-300 h-40 w-80 p-8 text-center rounded-lg justify-center items-center font-bold break-words">
+                    { fd?.name }
+                  </div>
+              </Link>
+            )
+          })
+        }
+
+      </div>
+      
 
     </div>
   )
 }
 
 export const revalidate = 300
+
