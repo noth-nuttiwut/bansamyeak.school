2e2e4bd8da3e769c681ddc1e1938588b2d354867
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 <nothsmac@Nuttiwuts-Mac-mini.local>
2023-09-22 15:59:16 +0700

    Add loading page

---
+++ app/vision-mission/loading.tsx
@@ -0,0 +1,6 @@
+
+export default function Loading() {
+    return <div className="flex justify-center w-full h-screen items-center bg-gradient-to-b from-amber-100 to-slate-50">
+        <span className="loading loading-ring w-16 md:w-24 3xl:w-48 text-error"></span>
+    </div>
+}
